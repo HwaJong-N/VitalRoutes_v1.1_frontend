cab30a67868e8d536f6f8b2aa123896fb472a5da
HwaJong <hwajong1016@gmail.com>
2024-03-27 02:51:00 +0900

    feat: 새로고침했을 때 로그인 유지

--- src/store/user/loginInfoStore.ts
+++ src/store/user/loginInfoStore.ts
@@ -8,23 +8,14 @@ interface LoginState extends LoginResponse {
   logout: () => void;
 }
 
-export const useLoginStore = create<LoginState>((set) => ({
-  isLogin: false,
+const localStorageKey = 'loginInfo';
 
-  memberId: null,
-  profile: null,
-  socialId: null,
-  name: null,
-  nickname: null,
-  email: null,
-  socialType: null,
-  accessToken: null,
-  refreshToken: null,
-
-  setLoginInfo: (data: LoginResponse) => set({ ...data }),
-  setIsLogin: (isLogin: boolean) => set({ isLogin }),
-  logout: () =>
-    set({
+export const useLoginStore = create<LoginState>((set) => {
+  // 로컬 스토리지에서 로그인 정보 불러오기
+  const storedLoginInfo = localStorage.getItem(localStorageKey);
+  const initialLoginState: LoginState = storedLoginInfo
+    ? { ...JSON.parse(storedLoginInfo), isLogin: true }
+    : {
       isLogin: false,
       memberId: null,
       profile: null,
@@ -35,5 +26,45 @@ export const useLoginStore = create<LoginState>((set) => ({
       socialType: null,
       accessToken: null,
       refreshToken: null,
-    }),
-}));
+    };
+
+  return {
+    ...initialLoginState,
+
+    // 로그인 정보 저장 및 로컬 스토리지에 저장하기
+    setLoginInfo: (data: LoginResponse) => {
+      set(data);
+      localStorage.setItem(localStorageKey, JSON.stringify(data));
+    },
+
+    // 로그인 상태 설정 및 로컬 스토리지에 저장하기
+    setIsLogin: (isLogin: boolean) => {
+      set({ isLogin });
+      const storedLoginInfo = localStorage.getItem(localStorageKey);
+      if (storedLoginInfo) {
+        const loginInfo = JSON.parse(storedLoginInfo);
+        localStorage.setItem(
+          localStorageKey,
+          JSON.stringify({ ...loginInfo, isLogin }),
+        );
+      }
+    },
+
+    // 로그아웃 처리 및 로컬 스토리지에서 로그인 정보 삭제하기
+    logout: () => {
+      set({
+        isLogin: false,
+        memberId: null,
+        profile: null,
+        socialId: null,
+        name: null,
+        nickname: null,
+        email: null,
+        socialType: null,
+        accessToken: null,
+        refreshToken: null,
+      });
+      localStorage.removeItem(localStorageKey);
+    },
+  };
+});
