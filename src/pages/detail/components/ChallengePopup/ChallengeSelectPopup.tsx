b59e9cd01152febe7ff7b62826db1b32b95f2097
HwaJong <hwajong1016@gmail.com>
2024-03-29 13:14:04 +0900

    feat: 챌린지 삭제 개발

---
+++ src/pages/detail/components/ChallengePopup/ChallengeSelectPopup.tsx
@@ -0,0 +1,67 @@
+import SelectionPopup from '@/components/common/SelectionPopup';
+import useCommentDeleteMutation from '@/hooks/challenge/useCommentDeleteMutation';
+import usePopup from '@/hooks/usePopup';
+import DeletePopup from './DeletePopup';
+import { storeFamilyCommentMode } from '@/store/challenge/commentStore';
+
+interface Props {
+  id: number;
+  memberId: number;
+}
+
+function ChallengeSelectPopup({ id, memberId }: Props) {
+  const useCommentModeStore = storeFamilyCommentMode(id);
+  const { mutate: mutateDeletion } = useCommentDeleteMutation(id);
+  const { setMode } = useCommentModeStore();
+  const { openPopup, closePopup } = usePopup();
+  const userInfo = localStorage.getItem('loginInfo');
+  let loginId = 0;
+
+  if (userInfo) {
+    const parsedInfo = JSON.parse(userInfo);
+    loginId = parsedInfo.memberId;
+  }
+
+  const deleteComment = () => {
+    openPopup(
+      <DeletePopup
+        onCancleClick={closePopup}
+    onConfirmClick={() => {
+      mutateDeletion();
+      closePopup();
+    }}
+    />,
+  );
+  };
+
+  const modifyComment = () => {
+    setMode('modify');
+  };
+
+  return (
+    <SelectionPopup>
+      {loginId === memberId ?
+      <>
+        <button type="button" onClick={modifyComment}>
+        수정하기
+        </button>
+        <button type="button" onClick={deleteComment}>
+    삭제하기
+    </button>
+    </>
+:
+  <>
+    <button type="button">
+    댓글 작성
+  </button>
+  <button type="button">
+    신고하기
+    </button>
+    </>
+}
+  </SelectionPopup>
+)
+  ;
+}
+
+export default ChallengeSelectPopup;
